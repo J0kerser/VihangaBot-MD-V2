de0823f20137b860774b44c9182a888cb984d6f6
JOKER_SER <87939150+J0kerser@users.noreply.github.com>
2022-08-24 22:38:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -21,16 +21,16 @@ global.OWNER_REACT = '🧑🏻‍💻' //මෙතනට emoji එකක දා�
 global.REACT_NUMBER = '9476342733' //මෙතන number එක.
 
 // Other
-global.owner = ['9476342733',''] //මෙතන number එක.
-global.premium = ['9476342733','9476342733'] //මෙතන number එක.
-global.botname = 'VIHANGA BOT INC' //මෙතන නම.
-global.ownername = 'Vihanga MD' //මෙතන නම.
-global.ownername = 'ᴠɪʜͥᴀɴͣɢͫᴀ ʏᴛ' //මෙතන නම.
-global.group1 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.group2 = 'https://chat.whatsapp.com/GTJmlChJZkoBhHVsENgc3Z' //මෙතන grouplink එක.
-global.packname = 'VIHANGA BOT INC' //මෙතන නම.
-global.cap = '*By Vihanga MD*' //මෙතන නම.
-global.img = 'https://telegra.ph/file/88d6fdeff08bbc4945901.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
+global.owner = ['919446697883',''] //මෙතන number එක.
+global.premium = ['919446697883','919446697883'] //මෙතන number එක.
+global.botname = 'JOKER BOT INC' //මෙතන නම.
+global.ownername = 'JOKER MD' //මෙතන නම.
+global.ownername = 'ᴊᴏᴋᴇʀ sᴇʀꥯ' //මෙතන නම.
+global.group1 = 'https://chat.whatsapp.com/C5v4Z7Nb7wl0DMrPWdFfLR' //මෙතන grouplink එක.
+global.group2 = 'https://chat.whatsapp.com/C5v4Z7Nb7wl0DMrPWdFfLR' //මෙතන grouplink එක.
+global.packname = 'JOKER BOT INC' //මෙතන නම.
+global.cap = '*By JOKERSER MD*' //මෙතන නම.
+global.img = 'https://telegra.ph/file/cf9132c36ae29ecb3e700.jpg' //මෙතන බොට්ගෙ photo එකට ඕන derect link එකක්.
 global.ytchannel = 'https://youtu.be/Z9rIR0Vopb8' //මෙතන කැමති ලින්ක් එකක්.
 global.monayawal = '100000' //මෙතන මාරු කරන්න එපා.
 global.darahawal = '100' //මෙතන මාරු කරන්න එපා.
@@ -52,7 +52,7 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip' //මෙතන මාරු කරන්න එපා.
 global.f5 = 'application/pdf' //මෙතන මාරු කරන්න එපා.
 global.f6 = 'application/vnd.android.package-archive' //මෙතන මාරු කරන්න එපා.
-global.author = 'VIHANGA BOT INC' //මෙතන නම.
+global.author = 'Joker BOT INC' //මෙතන නම.
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text=' //මෙතන මාරු කරන්න එපා.
 global.sessionName = 'session' //මෙතන මාරු කරන්න එපා.
 global.prefa = ['','!','.','🐦','🐤','🗿'] //මෙතන මාරු කරන්න එපා.
@@ -67,7 +67,7 @@ global.mess = {
     group: '*this feature is only for groups okay*',
     private: '*this feature can only used in dm okay*',
     bot: '*Special Features for User Bot Number okay*',
-    wait: '*ටිකක් ඉන්න ⏳*',
+    wait: '*Wait Setta⏳*',
     endLimit: '*Brothers Daily Limit Has Expired, Type .buy limit amount limit to buy limit, Note: Limit will be reset every 12 hours*',
 }
 global.limitawal = {
